c42b9b6bc897e82b2500119a66ebf435567b5326
El3ctric5had0w <julia.chwedczuk@gmail.com>
2023-10-25 14:06:35 +0200

    komentarz

---
+++ Nowy folder/lekcja 1 - struktura programu.cpp
@@ -0,0 +1,23 @@
+ #include <iostream>
+ using namespace std;
+ 
+ int main(){
+
+short liczba = 2;
+float zmiennoprzecinkowa = 3.532;
+string tekst = "Tekst";
+char znak = 'A';
+bool znakowy = false;
+
+cout << "Hello world" << endl;
+cout << "Liczba calkowita:" << liczba;
+cout << endl;
+
+string jakos; 
+cout << "Podaj jakis wyraz:";
+cin >> jakos;
+cout << "prowadziles wartosc:" << jakos;
+
+
+return 0;
+ }
