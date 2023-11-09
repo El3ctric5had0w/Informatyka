46bdbd7bb216ed8f48f4793a1909e676dbbacc1d
El3ctric5had0w <julia.chwedczuk@gmail.com>
2023-11-09 15:03:23 +0100

    komentarz

---
+++ Lekcje/Petle 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+	for (int i=1; i <= 10; i++){
+		for(int j=1; j <=10; j+=1){
+			cout << 1 << "*" << j << "=" << i*j << endl;
+			}
+
+	cout << endl;
+	}	
+
+
+
+
+
+cout << endl;
+system("pause");
+}
+
