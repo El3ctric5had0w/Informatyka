ebbc85e80a11bfb38434d4283d0061a5ff41db5f
El3ctric5had0w <julia.chwedczuk@gmail.com>
2023-11-15 14:13:04 +0100

    komentarz

---
+++ Lekcje/Petle Zadania.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+	short a, b;
+	cout <<"Podaj szerokosc";
+	cin >> a;
+	cout <<"Podaj wysokosc";
+	cin >> b;
+	cout << endl;
+		
+		for (int i = 0; i < b; i++) {
+				for (int j = 0; j < a; j++){
+					if(i == 0; i == b-1)
+					else if(j == 0; j == a-1)
+						cout << "*";
+					else
+					cout << "*";
+				
+				}
+		cout << endl;
+		}	
+cout << endl;
+system("pause");
+}
