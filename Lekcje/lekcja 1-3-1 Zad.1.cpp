58f9bf813df0f1c53c6ce94f9ba212ba18c2562e
El3ctric5had0w <julia.chwedczuk@gmail.com>
2023-11-30 15:05:24 +0100

    1-3-1

---
+++ Lekcje/lekcja 1-3-1 Zad.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout << "Wprowadz 5 liczb: ";
+cout << endl;
+
+int liczby[5];
+	for(int i=1; i < 4; i++){
+		cin >> liczby[i];
+	}
+	
+
+	
+	return 0;
+}
