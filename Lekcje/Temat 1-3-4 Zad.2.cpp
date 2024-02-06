ded8dc19e9196972880687ea539603eebcdfc939
El3ctric5had0w <julia.chwedczuk@gmail.com>
2024-02-06 10:10:01 +0100

    komentarz

---
+++ Lekcje/Temat 1-3-4 Zad.2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int Suma(int a, int b){
+	return a+b;
+}
+
+float Suma(float a, float b){
+	return a+b;
+}
+
+float Suma(float a, int b){
+	return a+b;
+}
+
+float Suma(int a, float b){
+	return a+b;
+}
+
+int main(){
+
+
+
+	return 0;
+}
