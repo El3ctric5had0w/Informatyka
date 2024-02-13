c5cb004b759156266ae4b20de281f9f91babd119
El3ctric5had0w <julia.chwedczuk@gmail.com>
2024-02-13 10:00:56 +0100

    komentarz

---
+++ Lekcje/temat 1-3-5 zad.2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+int telefon;    
+int *wsk;       
+    
+    cout << *wsk << endl;
+    
+    return 0;
+}
+
--- Lekcje/temat 1-3-5 zad.3.cpp
+++ Lekcje/temat 1-3-5 zad.3.cpp
@@ -1,4 +1,33 @@
 #include <iostream>
-usingnamespace std;
+using namespace std;
 
+void zwieksz_liczbe (float *liczba){
+	*liczba+= 1;
+}
 
+int main()
+{
+		float numer = 1;
+	float *wsk = &numer;
+	
+	cout << "wartosci zmiennych" << endl;
+	cout << "a: " << numer << endl;
+	cout << "b: " << numer << endl;
+	cout << "c: " << numer << endl;
+	
+	cout << "wartosci zmiennych po modyfikacji" << endl;
+	
+	zwieksz_liczbe(wsk);
+	
+	cout << "a: "<< numer << endl;
+	
+	zwieksz_liczbe(&numer);
+	
+	cout << "b: " << numer << endl;
+	
+	zwieksz_liczbe(&numer);
+	
+	cout << "c: "<< numer << endl;
+	
+	return 0;
+}
