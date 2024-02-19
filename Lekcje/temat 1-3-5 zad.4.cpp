062d2e1dbbd4a8e91c572cfcf897acf256332d74
El3ctric5had0w <julia.chwedczuk@gmail.com>
2024-02-19 14:10:06 +0100

    komentarz

---
+++ Lekcje/temat 1-3-5 zad.4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void int sumtab(int icz1, int licz2, int liczb3, int liczb4, int liczb5){
+	*liczb+
+}
+int main()
+{
+	int licz1 = 1, licz2 = 2, licz3 = 3, licz4 = 4, licz5 = 5;
+	int *wsk[5]
+
+    cout << "Podaj 5 liczb, ktore nalezy zsumowac: " << endl;
+    cout << "Suma liczb: " << sumtab << endl;
+    
+    return 0;
+}
