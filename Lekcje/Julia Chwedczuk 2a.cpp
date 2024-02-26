c086f80d62d7da8d058337285439fa7023be7938
El3ctric5had0w <julia.chwedczuk@gmail.com>
2024-02-26 14:10:12 +0100

    komentarz

---
+++ Lekcje/Julia Chwedczuk 2a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void liczby(char tablica);
+
+int main(){
+
+
+char tablica[10];
+cout <<"Podaj 10 losowych liczb: ";
+cin >> tablica[10];
+
+
+
+system("pause");
+}
